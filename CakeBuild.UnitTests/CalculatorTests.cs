b1fb6db055db25510851761ddf360f573d3498d0
unknown <ps@configit.com>
2017-11-27 18:19:58 +0000

    Add basic project

---
+++ CakeBuild.UnitTests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CakeBuild.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void When_adding_two_numbers__Then_the_result_is_correct()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add( 1, 2 );
+
+            Assert.That( result, Is.EqualTo( 3 ) );
+        }
+    }
+}
